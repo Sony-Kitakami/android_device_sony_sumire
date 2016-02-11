0a38a1c54597b24c70442db0a809b687f026a1c0
CTXz <ctx.xda@gmail.com>
2016-02-11 18:08:04 +0100

    Sumire: Add TWRP fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,9 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := device/sony/sumire
 
 PRODUCT_PLATFORM := kitakami
+
+# Build TWRP FSTAB
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
 
 include device/sony/kitakami/PlatformConfig.mk
 
@@ -16,3 +19,15 @@ DONT_DEXPREOPT_PREBUILTS := true
 BOARD_KERNEL_CMDLINE += androidboot.hardware=sumire
 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
+
+# TWRP flags
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_NO_USB_STORAGE := true
+TW_NO_SCREEN_BLANK := true
+
